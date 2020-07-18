1cb89b5bd35537a4fff7931700d93ec1e1238d1c
ron-hughes <ronald.hughes1982@gmail.com>
2020-07-18 14:44:45 -0400

    completed all tests, mvp

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,34 +1,114 @@
-const supertest = require("supertest")
-const server = require("../index")
-const db = require("../database/dbConfig")
-
+const supertest = require("supertest");
+const server = require("../index");
+const db = require("../database/dbConfig");
 
 afterAll(async () => {
-	// closes the database connection so the jest command doesn't stall
-	await db.destroy()
-})
+  // closes the database connection so the jest command doesn't stall
+  await db.destroy();
+});
 
+describe("the auth register endpoint", function () {
+  beforeEach(async () => {
+    await db("users").truncate();
+  }); // end truncate db
 
-
- describe('the auth register endpoint', function () {
-      beforeEach(async () => {
-        await db('users').truncate();
-      });// end truncate db
-
-      it('should return a status 201', function () {
-        return supertest(server)
-          .post('/api/auth/register')
-          .set('Content-Type', 'application/json')
-          .send({ "username": "papa smurf", "password": "password" })
-          .expect(201)
+  it("should return a status 201", function () {
+    return supertest(server)
+      .post("/api/auth/register")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .expect(201);
+  });
+  it("should return an object with message property", function () {
+    return supertest(server)
+      .post("/api/auth/register")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .then((res) => {
+        expect(res.body).toHaveProperty("message");
       });
-      it('should return an object with message property', function () {
+  });
+}); //end register endpoint
+
+describe("the auth login endpoint", function () {
+  it("should return a status 200", function () {
+    return supertest(server)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .expect(200);
+  });
+
+  it("should return an object with a  token property", function () {
+    return supertest(server)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .then((res) => {
+        expect(res.body).toHaveProperty("token");
+      });
+  });
+});
+
+describe("the jokes-router", function () {
+  beforeEach(async () => {
+    await db("users").truncate();
+  }); // end truncate db
+
+  it("should return a status 200", function () {
+    //register first
+    return supertest(server)
+      .post("/api/auth/register")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .then((res) => {
+        return res;
+      })
+      .then((res) => {
+        //log in first to get a token
         return supertest(server)
-          .post('/api/auth/register')
-          .set('Content-Type', 'application/json')
-          .send({ "username": "papa smurf", "password": "password" })
-          .then(res => {
-            expect(res.body).toHaveProperty('message')
+          .post("/api/auth/login")
+          .set("Content-Type", "application/json")
+          .send({ username: "thanos", password: "marvel" })
+          .then((res) => {
+            return res.body.token;
+          })
+          .then((token) => {
+            return supertest(server)
+              .get("/api/jokes")
+              .set("Content-Type", "application/json")
+              .set("Authorization", token)
+              .expect(200);
           });
       });
-    });//end register endpoint
+  });
+  it("should return an object with a joke property", function () {
+    //register first
+    return supertest(server)
+      .post("/api/auth/register")
+      .set("Content-Type", "application/json")
+      .send({ username: "thanos", password: "marvel" })
+      .then((res) => {
+        return res;
+      })
+      .then((res) => {
+        //log in first to get a token
+        return supertest(server)
+          .post("/api/auth/login")
+          .set("Content-Type", "application/json")
+          .send({ username: "thanos", password: "marvel" })
+          .then((res) => {
+            return res.body.token;
+          })
+          .then((token) => {
+            return supertest(server)
+              .get("/api/jokes")
+              .set("Content-Type", "application/json")
+              .set("Authorization", token)
+              .then((res) => {
+                expect(res.body[0]).toHaveProperty("joke");
+              });
+          });
+      });
+  });
+});
