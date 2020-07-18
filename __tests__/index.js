3ef4eeb8f273cd53ea9e408a5ba246833a24bb4d
ron-hughes <ronald.hughes1982@gmail.com>
2020-07-18 14:28:20 -0400

    one test passed :)

---
+++ __tests__/index.js
@@ -0,0 +1,25 @@
+const supertest = require("supertest")
+const server = require("../index")
+const db = require("../database/dbConfig")
+
+
+afterAll(async () => {
+	// closes the database connection so the jest command doesn't stall
+	await db.destroy()
+})
+
+
+
+ describe('the auth register endpoint', function () {
+      beforeEach(async () => {
+        await db('users').truncate();
+      });// end truncate db
+
+      it('should return a status 201', function () {
+        return supertest(server)
+          .post('/api/auth/register')
+          .set('Content-Type', 'application/json')
+          .send({ "username": "papa smurf", "password": "password" })
+          .expect(201)
+      });
+    });//end register endpoint
